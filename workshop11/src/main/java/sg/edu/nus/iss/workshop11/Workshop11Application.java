31a10d006b007ebed9c76a76268a8a0a7d1fa265
David Yap <davvyap@gmail.com>
2023-02-27 16:08:38 +0800

    Workshop 11 completed

--- workshop11/src/main/java/sg/edu/nus/iss/workshop11/Workshop11Application.java
+++ workshop11/src/main/java/sg/edu/nus/iss/workshop11/Workshop11Application.java
@@ -1,13 +1,52 @@
 package sg.edu.nus.iss.workshop11;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Workshop11Application {
 
+    private static String DEFAULT_PORT = "8888";
+    private static String port = null;
+
 	public static void main(String[] args) {
-		SpringApplication.run(Workshop11Application.class, args);
+        
+        // for(String argVal: args){
+        //     System.out.printf("Argument values: %s\n",argVal);
+        //     if(argVal.contains("--port=")){
+        //         port = argVal.substring(argVal.length() - 4, argVal.length());
+        //         System.out.printf("Port number: %s\n",port);
+        //     }
+        // }
+
+        // USE EXISTING CLASS TO CONDUCT THE SAME LOGIC AS ABOVE
+        ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+        // check if command line contains port number
+        if(cliOpts.containsOption("port")){
+            port = cliOpts.getOptionValues("port").get(0);
+        }
+
+        // IF WE ARE SETTING THE ENVIRONMENT VARIABLES USING CMD: set SERVER_PORT =3000
+        if(port == null){
+            port = System.getenv("SERVER_PORT");
+        }
+        
+        if(port == null || port.isBlank()){
+            port = DEFAULT_PORT;
+        }
+        System.out.printf("Port number: %s\n",port);
+
+        SpringApplication app = new SpringApplication(Workshop11Application.class);
+
+        app.setDefaultProperties(Collections.singletonMap("server.port", port));
+
+        System.out.println("Application is started on port >>> " + port);
+
+        app.run(args);
 	}
 
 }
